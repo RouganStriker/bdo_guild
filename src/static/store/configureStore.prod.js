eeabd68ef2e18a91f0b91c3a6f08cf8166f01608
Kelvin Chan <kchan@securitycompass.com>
2018-03-25 20:23:37 -0400

    Update prod store

--- src/static/store/configureStore.prod.js
+++ src/static/store/configureStore.prod.js
@@ -1,5 +1,6 @@
 import thunk from 'redux-thunk';
 import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga'
 import { routerMiddleware } from 'react-router-redux';
 
 import rootReducer from '../reducers';
@@ -7,8 +8,9 @@ import rootReducer from '../reducers';
 export default function configureStore(initialState, history) {
     // Add so dispatched route actions to the history
     const reduxRouterMiddleware = routerMiddleware(history);
+    const sagaMiddleware = createSagaMiddleware()
 
-    const middleware = applyMiddleware(thunk, reduxRouterMiddleware);
+    const middleware = applyMiddleware(thunk, reduxRouterMiddleware, sagaMiddleware);
 
     return createStore(rootReducer, initialState, middleware);
 }
