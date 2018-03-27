252dbac61effe155d4798938e282327ba7fcc2ae
Kelvin Chan <kchan@securitycompass.com>
2018-03-26 21:05:35 -0400

    Update 404 page

--- src/static/containers/NotFound/index.js
+++ src/static/containers/NotFound/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 export default class NotFoundView extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>NOT FOUND</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div style={{height: "100%", position: "absolute", width: "100%"}}>
+        <div style={{height: "100%", position: "relative", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column"}}>
+          <h1 style={{fontSize: "8rem"}}>404</h1>
+          <span>Return to whence you came</span>
+        </div>
+      </div>
+    );
+  }
 }
