1c6c2b9d66009925a5b944a3b0ce6bd0e4495358
Kelvin Chan <kchan@securitycompass.com>
2018-05-06 12:31:25 -0400

    Add help option to New Family Name page

--- src/static/containers/RequireFamilyName/index.js
+++ src/static/containers/RequireFamilyName/index.js
@@ -6,19 +6,29 @@ import { push } from 'react-router-redux';
 import Paper from 'material-ui/Paper';
 import { Field, reduxForm, submit } from 'redux-form';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
+import { ToastContainer, toast } from 'react-toastify';
 
 import { required } from '../../utils/validations';
 import { renderTextField } from '../../components/Fields'
 import BaseView from '../../components/BaseView'
 import Form from '../../components/Form'
 import {
-  AuthService,
+  ContactService,
   ProfileService,
 }  from '../../services'
 
 
 class RequireFamilyNameView extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showHelp: false,
+    }
+  }
+
   handleKeyPress(event) {
     const { dispatch } = this.props;
 
@@ -29,23 +39,24 @@ class RequireFamilyNameView extends React.Component {
     }
   }
 
-  render() {
+  renderForm() {
     const { handleSubmit } = this.props;
+    const { showHelp } = this.state;
 
     return (
-      <Dialog open={true}
-              model={true}
-              title={`Welcome to BDO Guilds, ${user.discord_id}!`}
-              actions={[<FlatButton
-                label="Submit"
-                primary={true}
-                onClick={() => this.props.dispatch(submit('profile'))}
-              />]}>
-        <p>
-          Please enter in your BDO account's Family Name.
-          All statistics will be associated with the family name. You cannot change
-          this value once it is set.
-        </p>
+      <div>
+        {
+          !showHelp &&
+          <p>
+            Please enter in your BDO account's Family Name.
+            All statistics will be associated with the family name. You cannot change
+            this value once it is set.
+          </p> ||
+          <p>
+            If your family name is already taken, you may submit a request to dispute it.
+            You will be contacted by support once the request has been received.
+          </p>
+        }
         <Form onSubmit={handleSubmit}>
           <Field name="family_name"
                  component={renderTextField}
@@ -54,27 +65,81 @@ class RequireFamilyNameView extends React.Component {
                  validate={[required]}
                  fullWidth={true}
                  onKeyPress={this.handleKeyPress.bind(this)} />
+        { showHelp &&
+          <Field name="email"
+                 component={renderTextField}
+                 label="Contact Email"
+                 className="form-field"
+                 validate={[required]}
+                 fullWidth={true}
+                 onKeyPress={this.handleKeyPress.bind(this)} />
+        }
         </Form>
-      </Dialog>
+      </div>
+    )
+  }
+
+  render() {
+    const { showHelp } = this.state;
+    const actions = [
+      <FlatButton label={showHelp && "Back" || "Help"}
+                  style={{float: "left"}}
+                  onClick={() => this.setState({showHelp: !showHelp})} />,
+      <RaisedButton label="Submit"
+                    primary={true}
+                    onClick={() => this.props.dispatch(submit('profile'))} />
+    ];
+
+    return (
+      <div>
+        <Dialog open={true}
+                model={true}
+                title={`Welcome to BDO Guilds, ${user.discord_id}!`}
+                actions={actions}>
+          { this.renderForm() }
+        </Dialog>
+        <ToastContainer hideProgressBar={true}
+                        position="bottom-right" />
+      </div>
     );
   }
 }
 
 function submitForm(values, dispatch, props) {
-  const { id, family_name } = values
+  const { id, family_name, email } = values
+  const { registeredFields } = props;
 
-  dispatch(ProfileService.create({
-    payload: { family_name },
-    form: 'profile',
-    onSuccess: (result) => {
-      window.location.assign('/settings');
-    },
-  }));
+  if (registeredFields.email) {
+    // We are showing Help view
+    dispatch(ContactService.create({
+      payload: {
+        category: "account",
+        email,
+        summary: "Duplicate Family Name",
+        description: `The family name '${family_name}' is unavailable.`,
+      },
+      form: 'profile',
+      onSuccess: (result) => {
+        toast.success("Your request has been submitted.")
+      },
+    }));
+  } else {
+    dispatch(ProfileService.create({
+      payload: { family_name },
+      form: 'profile',
+      onSuccess: (result) => {
+        window.location.assign('/settings');
+      },
+    }));
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
     user: state.auth.user,
+    initialValues: {
+      email: state.auth.user.email,
+    }
   };
 };
 
