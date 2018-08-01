e9e93891d85d37e2c7951c6e504b2ee61f52575e
Kelvin Chan <kchan@securitycompass.com>
2018-08-01 01:07:50 -0400

    Fix typo

--- src/api/views/war.py
+++ src/api/views/war.py
@@ -47,7 +47,7 @@ class WarViewSet(ModelViewSet, GuildViewMixin):
     include_params = ['stats']
 
     def get_queryset(self):
-        return super(WarViewSet, self).select_related('node')
+        return super(WarViewSet, self).get_queryset().select_related('node')
 
     def get_serializer_context(self):
         context = super(WarViewSet, self).get_serializer_context()
